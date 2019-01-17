f4254a713d7f1dedf1aeae963f3432582f6fb9e0
jaydedm <realguitarjayde@gmail.com>
2019-01-17 12:31:47 -0600

    finished pluralsight demo3

--- Pluralsight Demos/CreateAndPopulate/CreateAndPopulate/Main.java
+++ Pluralsight Demos/CreateAndPopulate/CreateAndPopulate/Main.java
@@ -1,13 +1,19 @@
 package CreateAndPopulate;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +29,10 @@ public class Main {
     };
 
         try(FileSystem zipFs = openZip(Paths.get("myData.zip"))) {
+            copyToZip(zipFs);
+            writeToFileInZip1(zipFs, data);
+            writeToFileInZip2(zipFs, data);
+
         } catch (Exception e) {
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
         }
@@ -40,7 +50,24 @@ public class Main {
     }
 
     private static void copyToZip(FileSystem zipFs) throws IOException {
+        Path sourceFile = Paths.get("file1.txt");
+       //Path sourceFile = FileSystems.getDefault().getPath("file1.txt");
+        Path destFile = zipFs.getPath("/file1Copied.txt");
 
 
+        Files.copy(sourceFile, destFile, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void writeToFileInZip1(FileSystem zipFs, String[] data) throws IOException {
+        try(BufferedWriter writer = Files.newBufferedWriter(zipFs.getPath("/newFile1.txt"))) {
+            for(String d:data) {
+                writer.write(d);
+                writer.newLine();
+            }
+        }
+    }
+
+    private static void writeToFileInZip2(FileSystem zipFs, String[] data) throws IOException {
+        Files.write(zipFs.getPath("/newFile2.txt"), Arrays.asList(data), Charset.defaultCharset(), StandardOpenOption.CREATE);
     }
 }
